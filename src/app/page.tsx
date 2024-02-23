752ae73bcf190748e32a0b0d3bd64c45db449830
Magnus07 <stanislav.pinchuk.02@gmail.com>
2024-02-23 21:44:12 +0200

    config: add clerk config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-red-500">Hello world</h1>;
+  return <Button>Hello</Button>;
 }
